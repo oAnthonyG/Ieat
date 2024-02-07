f1a1029177dac0d6eb0a6ec278864c1e53e73cea
oAnthonyG <anthonytguilherme@hotmail.com>
2024-02-07 13:31:41 -0300

    created a restaurant products section

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_180358) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_07_161255) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.float "value"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
